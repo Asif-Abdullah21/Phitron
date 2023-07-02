c3de83ff37df6eb06ab02255811d1e6ef7d37567
Asif-Abdullah21 <asifabdullah214365@gmail.com>
2023-07-02 23:24:59 +0600

    Queue Implement using Singly Linked List

---
+++ Phitron/Semester 2/Basic Data Structure/Module 14/1_Queue_Implement_using_Singly_Linked_List.cpp
@@ -0,0 +1,117 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node
+{
+    public:
+    int val;
+    Node * next;
+
+    Node(int val)
+    {
+        this->val = val;
+        this->next = NULL;
+    }
+};
+
+class myQueue
+{
+    public:
+    Node *head = NULL;    
+    Node *tail = NULL;  
+    int sz = 0;  
+
+    void push(int val)
+    {
+        sz++;
+        Node *newNode = new Node(val);
+        if(head==NULL)
+        {
+            head = newNode;
+            tail = newNode;
+            return;
+        }
+
+        tail->next = newNode;
+        tail = newNode;
+    }
+
+    void pop()
+    {
+        sz--;
+        Node * del = head;
+        head = head->next;
+        delete del;
+        if(head == NULL) tail = NULL; // ekta value thakle eita handle kortese
+    }
+
+    int front()
+    {
+        return head->val;
+    }
+
+    int size()
+    {
+        return sz;
+    }
+
+    bool empty()
+    {
+        if(sz==0) return true;
+        else return false;
+    }
+};
+
+int main()
+{ 
+    myQueue q;
+    int n;
+    cin >> n;
+
+    for (int i = 0; i < n; i++)
+    {
+        int x;
+        cin >> x;
+        q.push(x);
+    }
+
+    while (!q.empty())
+    {
+        cout << q.front() << " ";
+        q.pop();
+    }
+    
+    
+    
+
+    return 0;
+}
+
+
+/*
+for single values:
+
+int main()
+{ 
+    myQueue q;
+
+    q.push(10);
+    q.push(20);
+    q.push(30);
+    
+    cout << q.front() << " ";
+    q.pop();
+    
+    cout << q.front() << " ";
+    q.pop();
+    
+    cout << q.front() << endl; // front access er age ba pop korar age check kre nite hbe je queue ta empty ki na
+    q.pop();
+    // q.pop();abar dile Segmentation fault asbe jehetu r value nai
+
+    return 0;
+}
+
+
+
+*/
