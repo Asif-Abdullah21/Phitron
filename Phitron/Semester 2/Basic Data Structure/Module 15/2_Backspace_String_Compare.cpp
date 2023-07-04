ec03f10c5fdb7139b2ab9337fb9484d91c38a7c7
Asif-Abdullah21 <asifabdullah214365@gmail.com>
2023-07-05 02:44:36 +0600

    844. Backspace String Compare

---
+++ Phitron/Semester 2/Basic Data Structure/Module 15/2_Backspace_String_Compare.cpp
@@ -0,0 +1,60 @@
+class Solution {
+public:
+    bool backspaceCompare(string s, string t) {
+        stack<char> st1,st2;
+        //stack<char> st2;
+
+        for(char ch:s)
+        {
+            if(ch=='#')
+            {
+                if(!st1.empty()) st1.pop();
+            }
+            else st1.push(ch);
+        }
+
+        for(char ch:t)
+        {
+            if(ch=='#')
+            {
+                if(!st2.empty()) st2.pop();
+            }
+            else st2.push(ch);
+        }
+
+        if(st1==st2) return true;
+        else return false;
+    }
+};
+
+
+
+//----------------------------------------------------------------------------------
+
+
+
+
+// class Solution {
+// public:
+//     bool backspaceCompare(string s, string t) {
+//         stack<char> st1,st2;
+//         //stack<char> st2;
+
+//         for(char ch:s)
+//         {
+//             if(ch!='#') st1.push(ch);
+//             else if(st1.empty()==false) st1.pop();
+ 
+//         }
+
+//         for(char ch:t)
+//         {
+//             if(ch!='#') st2.push(ch);
+//             else if(st2.empty()==false) st2.pop(); // else if a asha manei # chilo
+//         }
+
+//         if(st1==st2) return true;
+//         else return false;
+//     }
+// };
+
