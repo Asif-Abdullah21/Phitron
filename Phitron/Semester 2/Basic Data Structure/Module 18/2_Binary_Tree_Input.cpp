48564f45de642a6412051f46270ad5e0d9719ef1
Asif-Abdullah21 <asifabdullah214365@gmail.com>
2023-07-11 00:45:55 +0600

    Binary tree input

--- Phitron/Semester 2/Basic Data Structure/Module 18/2_Binary_Tree_Input.cpp
+++ Phitron/Semester 2/Basic Data Structure/Module 18/2_Binary_Tree_Input.cpp
@@ -18,17 +18,18 @@ Node *input_tree()
     int val;
     cin >> val;
     Node *root;
-    if (val == -1)
-        root = NULL;
-    else
-        root = new Node(val);
+
+    if (val == -1)root = NULL;
+    else root = new Node(val);
+
     queue<Node *> q;
-    if (root)
-        q.push(root);
+
+    if (root) q.push(root); //     if (root!=NULL) q.push(root);
+    
     while (!q.empty())
     {
         // 1. ber kore ano
-        Node *p = q.front();
+        Node *p = q.front(); //p = parent
         q.pop();
 
         // 2. jabotiyo ja kaj ache
@@ -49,10 +50,8 @@ Node *input_tree()
         p->right = myRight;
 
         // 3. children gulo ke push koro
-        if (p->left)
-            q.push(p->left);
-        if (p->right)
-            q.push(p->right);
+        if (p->left)    q.push(p->left); //   same as ->  if (p->left!=NULL)    q.push(p->left);
+        if (p->right)  q.push(p->right); // cz null false return kre
     }
     return root;
 }
