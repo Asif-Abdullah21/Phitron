cb9348d563cf56632b2a2cd451f6cb63abed05a3
Asif-Abdullah21 <asifabdullah214365@gmail.com>
2023-10-25 23:24:42 +0600

    Bitmasking

---
+++ Phitron/XPSC/BitMasking/mask.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+#define endl "\n"
+#define nn (cout << "NO\n")
+#define yy (cout << "YES\n")
+#define ll long long
+#define ull unsigned long long
+#define gcd(a, b) __gcd(a, b)
+#define lcm(a, b) ((a / gcd(a, b)) * b)
+#define pii pair<int, int>
+#define pll pair<long long, long long>
+#define mm(a, x) memset(a, x, sizeof(a))
+#define FIO                       \
+    ios_base::sync_with_stdio(0); \
+    cin.tie(0);                   \
+    cout.tie(0);
+using namespace std;
+
+int main()
+{
+    FIO;
+    int n,k; cin >> n >> k;
+
+    // n er kth bit 0 naki one dekhte chai, indexing last theke
+
+    int mask = 1<<k;
+
+    if((mask & n)==0)  cout << 0 << endl;
+    else cout << 1 << endl;
+     
+    return 0;
+}
